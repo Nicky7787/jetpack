8a4c7589efb591238482a836518ad8c210c14276
Igor Zinovyev <zinigor@gmail.com>
2019-12-12 19:25:21 +0300

    Add missing priority to the later hook.

--- packages/config/src/class-config.php
+++ packages/config/src/class-config.php
@@ -47,7 +47,7 @@ class Config {
 
 		add_action( 'plugins_loaded', array( $this, 'on_plugins_loaded_early' ), 5 );
 		add_action( 'plugins_loaded', array( $this, 'on_plugins_loaded' ) );
-		add_action( 'plugins_loaded', array( $this, 'on_plugins_loaded_late' ) );
+		add_action( 'plugins_loaded', array( $this, 'on_plugins_loaded_late' ), 90 );
 	}
 
 	/**
